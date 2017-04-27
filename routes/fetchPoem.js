f6e788a900b7d191087b7c5fa5146681d96dde7c
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-04-27 14:10:33 -0400

    Better logging, order poems by ID when returning

--- routes/fetchPoem.js
+++ routes/fetchPoem.js
@@ -2,30 +2,30 @@ var express = require('express')
 var router = express.Router()
 require('dotenv').config()
 var pg = require('pg')
-pg.defaults.ssl = true
+//pg.defaults.ssl = true
 var knex = require('knex')({
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: process.env.LOCAL_DATABASE_URL,
   searchPath: 'knex,public'
 })
 
 router.get('/random', function(req, res) {
-  console.log('received request for random poem')
   knex('poems')
   .select('*')
   .where({ completed: 'false' })
   .then(function(rows){
     var randomPoem = rows[Math.floor(Math.random() * rows.length)]
-    debugger
+    console.log('Returned poem',randomPoem.id)
     res.send(randomPoem)
   })
 })
 router.get('/completed', function(req, res) {
-  console.log('received request for completed poems')
   knex('poems')
   .select('*')
   .where({ completed: 'true' })
+  .orderBy('id', 'desc')
   .then(function(rows){
+    console.log('Returned',rows.length,'completed poems')
     res.send(rows)
   })
 })
