590c9c4843021e12d5b675c1835db5564599262b
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-10-10 15:32:34 -0700

    Working connect() on LoadMoreButton\!

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,24 @@ var PoemContainer = require('./PoemContainer')
 var Editor = require('./Editor')
 var api = require('../../utils/api')
 import { Glyphicon, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+import { fetchCompletedPoems } from '../redux/actions/completedPoems.js'
+import rootReducer from '../redux/reducers/reducers.js'
+import { Provider } from 'react-redux'
 
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware, // lets us dispatch() functions
+    loggerMiddleware // neat middleware that logs actions
+  )
+)
+
+store.dispatch(fetchCompletedPoems())
 
 class App extends React.Component {
   constructor(props) {
@@ -53,26 +70,28 @@ class App extends React.Component {
   render() {
     const longEnoughTooltip = (<Tooltip id='long_enough_explanation'>Requiring a minimum poem length has lead to more interesting poems and less trolling.</Tooltip>)
     return (
-      <div className='app'>
-        <h1>Exquisite Corpse</h1>
-        <Editor
-          refreshCompletedPoems={this.refreshCompletedPoems}
-          refreshPoemCounts={this.refreshPoemCounts}
-          uncompletedcount={this.state.uncompletedcount}
-          completedcount={this.state.completedcount}
-        />
-        <h4>What is this?</h4>
-        <p>Collective, anonymous Internet poetry, written one line at a time.  Anybody can contribute a line, seeing only the one that came before.  Once a poem is long enough,
-        <OverlayTrigger
-          overlay={longEnoughTooltip}
-          placement='top'>
-          <sup><Glyphicon glyph='question-sign' /></sup>
-        </OverlayTrigger>
-        &nbsp;you can also choose to end it, at which point the whole thing becomes public.</p>
-        <p>Exquisite Corpse began as a Surrealist parlor game in the early 20th century. I created this Internet version in 2008.  Read about its various incarnations (as it were) <a href="http://harpojaeger.com/2017/05/10/exquisite-corpse" target="_blank">here</a>, and find the tech specs/fine print <a href="https://github.com/harpojaeger/exquisite-corpse/blob/master/README.md">here</a>.</p>
-        <p>Made by <a href="http://harpojaeger.com">Harpo Jaeger</a>.</p>
-        <PoemContainer poems={this.state.poems}/>
-      </div>
+      <Provider store={store}>
+        <div className='app'>
+          <h1>Exquisite Corpse</h1>
+          <Editor
+            refreshCompletedPoems={this.refreshCompletedPoems}
+            refreshPoemCounts={this.refreshPoemCounts}
+            uncompletedcount={this.state.uncompletedcount}
+            completedcount={this.state.completedcount}
+          />
+          <h4>What is this?</h4>
+          <p>Collective, anonymous Internet poetry, written one line at a time.  Anybody can contribute a line, seeing only the one that came before.  Once a poem is long enough,
+            <OverlayTrigger
+              overlay={longEnoughTooltip}
+            placement='top'>
+              <sup><Glyphicon glyph='question-sign' /></sup>
+            </OverlayTrigger>
+          &nbsp;you can also choose to end it, at which point the whole thing becomes public.</p>
+          <p>Exquisite Corpse began as a Surrealist parlor game in the early 20th century. I created this Internet version in 2008.  Read about its various incarnations (as it were) <a href="http://harpojaeger.com/2017/05/10/exquisite-corpse" target="_blank">here</a>, and find the tech specs/fine print <a href="https://github.com/harpojaeger/exquisite-corpse/blob/master/README.md">here</a>.</p>
+          <p>Made by <a href="http://harpojaeger.com">Harpo Jaeger</a>.</p>
+          <PoemContainer poems={this.state.poems}/>
+        </div>
+      </Provider>
     )
   }
 }
--- client/src/components/PoemContainer.js
+++ client/src/components/PoemContainer.js
@@ -4,6 +4,7 @@ var Loader = require('./Loader')
 var PropTypes = require('prop-types')
 import { Button } from 'react-bootstrap'
 import '../styles/PoemContainer.css'
+import ConnectedLoadMoreButton from './LoadMoreButton.js'
 
 class PoemContainer extends React.Component {
   constructor(props) {
@@ -57,26 +58,12 @@ class PoemContainer extends React.Component {
   }
 
   render() {
-    var numPoemsToLoad = this.state.quantity
-    if(this.state.to+this.state.quantity > this.state.poems.length) {
-      numPoemsToLoad = this.state.poems.length - this.state.to
-    }
     return (
       <div className='poem-container'>
         <h3>Completed poems</h3>
         <Loader visible={ this.props.poems.length === 0 }>Loading...</Loader>
         <PoemList poems={this.state.poems.slice(this.state.from,this.state.to)} />
-        <Button
-          type='submit'
-          name='action'
-          onClick={this.loadMorePoems}
-          value='add'
-          disabled={ this.state.to >= this.state.poems.length }
-          className={ 'load-more ' + (this.state.to >= this.state.poems.length && 'hidden') }
-          >
-          Load {numPoemsToLoad} more poem{ numPoemsToLoad > 1 && 's' }
-          {/* {this.state.nextids.first} - {this.state.nextids.last} */}
-        </Button>
+        <ConnectedLoadMoreButton />
       </div>
     )
   }
