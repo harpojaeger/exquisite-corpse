0acf693c4e2d49be11b59e1420e40864c1e8245e
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-10-12 10:07:53 -0700

    Split <CompletedPoem /> out into own component

--- client/src/components/PoemList.js
+++ client/src/components/PoemList.js
@@ -1,57 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import dateFormat from 'dateformat'
-import { Glyphicon } from 'react-bootstrap'
 import { connect } from 'react-redux'
-const Entities = require('html-entities').AllHtmlEntities
-const entities = new Entities()
 import '../styles/PoemList.css'
-
-function Timestamp(props) {
-  return(
-    <span className='timestamp'>
-      {dateFormat(new Date(props.children*1),"mmm d, yyyy")}
-    </span>
-  )
-}
-
-function Poem(props) {
-  return (
-    <li id={props.id} key={props.id} className='poem'>
-      <div className='meta'>
-
-        Poem #{props.id}
-        {props.starttime &&
-          <span> (<Timestamp>{props.starttime}</Timestamp></span>
-        }
-        {props.endtime &&
-          <span> – <Timestamp>{props.endtime}</Timestamp></span>
-        }
-        {props.starttime && ')'}
-        <a href={'#'+props.id} className='hover-link'>
-          <Glyphicon glyph='link' />
-        </a>
-        <a className='hover-link' href='#top'>
-          <Glyphicon glyph='circle-arrow-up' />
-        </a>
-      </div>
-      {props.lines.map( (line, index) =>
-        <p className='line' key={index}>{entities.decode(line)}</p>
-      )}
-    </li>
-  )
-}
-Poem.propTypes = {
-  id: PropTypes.number.isRequired,
-  starttime: PropTypes.string,
-  endtime: PropTypes.string,
-  lines: PropTypes.array.isRequired,
-}
-
-Poem.defaultProps = {
-  starttime: '',
-  endtime: '',
-}
+import Poem from './CompletedPoem'
 
 function PoemList(props) {
   return(
