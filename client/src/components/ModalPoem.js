4d43ac82f683b2de58a29826c6e12a4609511268
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-10-12 14:16:26 -0700

    Load <ModalPoem> when request with hash

--- client/src/components/ModalPoem.js
+++ client/src/components/ModalPoem.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
-import { Modal } from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import Poem from './Poem'
 
-function ModalPoem(props){
-  let thispoem = props.poems[1]
-  console.log('thispoem is',thispoem)
-  console.log('ModalPoem received props', props)
-  return(
-    <Modal show={true}>
-      <Poem id={thispoem.id} starttime={thispoem.starttime} endtime={thispoem.endtime} lines={thispoem.lines} />
-    </Modal>
-  )
+class ModalPoem extends Component{
+  constructor(props){
+    super(props)
+    this.state={ showModal: true }
+    this.close = this.close.bind(this)
+  }
+  componentWillReceiveProps(){
+    this.setState( { showModal: true} )
+  }
+  close() {
+    this.setState({ showModal: false });
+  }
+  render(){
+    let thispoem = this.props.poems.find( (poem) => poem.id === this.props.id )
+    console.log('thispoem is',thispoem)
+    console.log('ModalPoem received props', this.props)
+    return(
+      <Modal show={this.state.showModal} onHide={this.close}>
+        <Poem id={thispoem.id} starttime={thispoem.starttime} endtime={thispoem.endtime} lines={thispoem.lines} />
+        <Modal.Footer>
+          <Button onClick={this.close}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 }
 
 ModalPoem.propTypes = {
-  id: PropTypes.number,
+  id: PropTypes.number.isRequired,
   poems: PropTypes.array.isRequired,
+}
+
+const ModalPoemContainer = (props) => {
+  return(
+    <div>
+      {props.poems.length>1 &&
+        <ModalPoem id={props.id} poems={props.poems}/>}
+    </div>
+  )
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -28,8 +53,8 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-const ConnectedModalPoem = withRouter(connect(
+const ConnectedModalPoemContainer = withRouter(connect(
   mapStateToProps
-)(ModalPoem))
+)(ModalPoemContainer))
 
-export default ConnectedModalPoem
+export default ConnectedModalPoemContainer
