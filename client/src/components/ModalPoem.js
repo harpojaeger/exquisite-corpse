f126537162e85ac8c1ac3a6fcc39c0afb719b576
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-10-12 13:47:58 -0700

    Use HashRouter to display modal

--- client/src/components/ModalPoem.js
+++ client/src/components/ModalPoem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 import { Modal } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import Poem from './Poem'
@@ -7,27 +8,28 @@ import Poem from './Poem'
 function ModalPoem(props){
   let thispoem = props.poems[1]
   console.log('thispoem is',thispoem)
+  console.log('ModalPoem received props', props)
   return(
-    <Modal>
+    <Modal show={true}>
       <Poem id={thispoem.id} starttime={thispoem.starttime} endtime={thispoem.endtime} lines={thispoem.lines} />
     </Modal>
   )
 }
 
 ModalPoem.propTypes = {
-  id: PropTypes.number.isRequired,
+  id: PropTypes.number,
   poems: PropTypes.array.isRequired,
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
   return {
     poems: state.poems,
-    id: state.id
+    id: parseInt(ownProps.match.params.id,10),
   }
 }
 
-const ConnectedModalPoem = connect(
+const ConnectedModalPoem = withRouter(connect(
   mapStateToProps
-)(ModalPoem)
+)(ModalPoem))
 
 export default ConnectedModalPoem
--- client/src/components/PoemList.js
+++ client/src/components/PoemList.js
@@ -3,12 +3,10 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import '../styles/PoemList.css'
 import Poem from './Poem'
-import ConnectedModalPoem from './ModalPoem'
 
 function PoemList(props) {
   return(
     <ul className='no-bullets no-padding poem-list'>
-      {props.displayModal && <ConnectedModalPoem /> }
       {props.poems.map( (poem) =>
         <li id={poem.id} key={poem.id} className='poem'>
           <Poem key={poem.id} id={poem.id} starttime={poem.starttime} endtime={poem.endtime} lines={poem.lines}
