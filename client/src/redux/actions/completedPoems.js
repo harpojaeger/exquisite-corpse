acd429ce25621a1c6c433bfb22b87eee2987b256
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-10-20 16:59:29 -0700

    Action creators & reducer for setting sort options

--- client/src/redux/actions/completedPoems.js
+++ client/src/redux/actions/completedPoems.js
@@ -2,6 +2,12 @@ const api = require('../../../utils/api')
 
 export const DISPLAY_MORE_POEMS = 'DISPLAY_MORE_POEMS'
 export const RECEIVE_COMPLETED_POEMS = 'RECEIVE_COMPLETED_POEMS'
+export const SET_SORT_BY = 'SET_SORT_BY'
+export const START_DATE = 'START'
+export const END_DATE = 'END'
+export const ASC = 'ASC'
+export const DESC = 'DESC'
+export const SET_SORT_ORDER = 'SET_SORT_ORDER'
 
 export function fetchCompletedPoems(){
   return function(dispatch){
@@ -24,4 +30,12 @@ export function loadPoemByID(history, id){
   return function(dispatch){
     history.push(id)
   }
+}
+
+export function setSortBy(attribute){
+  return { type: SET_SORT_BY, attribute: attribute }
+}
+
+export function setSortOrder(order){
+  return { type: SET_SORT_ORDER, order: order }
 }
--- client/src/redux/reducers/reducers.js
+++ client/src/redux/reducers/reducers.js
@@ -1,5 +1,5 @@
 // Import action types
-import { RECEIVE_COMPLETED_POEMS, DISPLAY_MORE_POEMS } from '../actions/completedPoems.js'
+import { RECEIVE_COMPLETED_POEMS, DISPLAY_MORE_POEMS, SET_SORT_ORDER, SET_SORT_BY, START_DATE, END_DATE, ASC, DESC } from '../actions/completedPoems.js'
 import { UPDATE_UNCOMPLETED_COUNT, UPDATE_COMPLETED_COUNT } from '../actions/poemCounts.js'
 import { FETCH_NEW_PROMPT, PROMPT_REFRESHED, NEW_POEM_CREATED } from '../actions/editing.js'
 
@@ -19,6 +19,10 @@ const initialState = {
   // Current number of completed & uncompleted poems
   completed: 0,
   uncompleted: 0,
+
+  // Sorting options for completed poems
+  sort_by: END_DATE,
+  sort_order: DESC
 }
 
 function app(state = initialState, action) {
@@ -54,7 +58,18 @@ function app(state = initialState, action) {
       })
     case NEW_POEM_CREATED:
       return state
-
+    case SET_SORT_BY:
+      // Check to see that the sort attribute is valid
+      if(![START_DATE, END_DATE].includes(action.attribute)) return state
+      return Object.assign({}, state, {
+        sort_by: action.attribute
+      })
+    case SET_SORT_ORDER:
+      // Check to see that the sort order is valid
+      if(![ASC, DESC].includes(action.order)) return state
+      return Object.assign({}, state, {
+        sort_order: action.order
+      })
     default:
       return state
   }
