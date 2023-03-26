fbaf9185f9fe160f570627757d24c177ee65b9e2
Harpo Jaeger <harpo.jaeger@gmail.com>
2023-03-26 14:31:17 -0400

    poem implements Poem

--- server/internal/poem/poem.go
+++ server/internal/poem/poem.go
@@ -9,7 +9,7 @@ import (
 // Poem represents a single poem in the Exquisite Corpse project. It may be
 // in-progress or completed.
 type Poem interface {
-	Completed() bool
+	IsCompleted() bool
 	Lines() []string
 	StartTime() time.Time
 	CompleteTime() time.Time
@@ -20,6 +20,31 @@ type Poem interface {
 // poem is an internal implementation of Poem, using GORM.
 type poem struct {
 	gorm.Model
-	lines     []string
-	completed bool
+	lines        []string
+	complete     bool
+	completeTime time.Time
+}
+
+func (p *poem) IsCompleted() bool {
+	return p.complete
+}
+
+func (p *poem) Lines() []string {
+	return p.lines
+}
+
+func (p *poem) StartTime() time.Time {
+	return p.CreatedAt
+}
+
+func (p *poem) CompleteTime() time.Time {
+	return p.completeTime
+}
+
+func (p *poem) AddLine(string) error {
+	return nil
+}
+
+func (p *poem) Complete() error {
+	return nil
 }
